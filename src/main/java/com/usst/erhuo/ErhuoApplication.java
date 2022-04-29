f4ad4d43008ff0b9f5f158f3e2324f6dd1e90c86
huangyuan <huangyuan_email@163.com>
2022-04-29 22:20:18 +0800

    test

---
+++ src/main/java/com/usst/erhuo/ErhuoApplication.java
@@ -0,0 +1,13 @@
+package com.usst.erhuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErhuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErhuoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usst/erhuo/ErhuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usst.erhuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErhuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
