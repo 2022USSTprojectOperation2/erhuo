6913a9f4cb38773a286422d70126f6974d3b6db7
huangyuan <1031608160@qq.com>
2022-05-11 15:30:28 +0800

    test

--- src/main/java/com/usst/erhuo/controller/UserController.java
+++ src/main/java/com/usst/erhuo/controller/UserController.java
@@ -5,9 +5,11 @@ import com.usst.erhuo.entity.User;
 import com.usst.erhuo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
@@ -18,8 +20,9 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/all")
-    public List<User> getAllUser(){
+    @PostMapping("/all")
+    public List<User> getAllUser(String name, HttpSession session){
+        //System.out.println(name);
         return userService.getAllUser();
     }
 }
