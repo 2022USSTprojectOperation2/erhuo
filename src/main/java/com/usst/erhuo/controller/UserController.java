f687043a180c397cb7c68684988f17005d2a4d0b
Pvneuma <310256126@qq.com>
2022-05-21 15:31:34 +0800

    修改UserController部分接口

--- src/main/java/com/usst/erhuo/controller/UserController.java
+++ src/main/java/com/usst/erhuo/controller/UserController.java
@@ -4,16 +4,13 @@ package com.usst.erhuo.controller;
 import com.usst.erhuo.entity.User;
 import com.usst.erhuo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     UserService userService;
@@ -26,23 +23,23 @@ public class UserController {
 
     //用户登录
     @RequestMapping("/login")
-    public Integer userLogin(String userName,String password,HttpSession session){
-        Integer userId = userService.userLogin(userName,password);
+    public Integer userLogin(@RequestBody User user, HttpSession session){
+        Integer userId = userService.userLogin(user.getUserName(),user.getPassword());
         if(userId!=null) session.setAttribute("userId",userId);
         return userId;
     }
 
     //验证注册时用户名是否唯一
-    @RequestMapping("/checkUser")
-    public Integer checkUserName(String userName){
+    @RequestMapping("/checkUser/{userName}")
+    public Integer checkUserName(@PathVariable String userName){
         return userService.checkUserLogin(userName);
     }
 
     //用户注册
     @RequestMapping("/register")
-    public Integer userRegister(String userName,String password,String phone,HttpSession session){
-        userService.userRegister(userName,password,phone);
-        Integer userId = userService.userLogin(userName,password);
+    public Integer userRegister(@RequestBody User user,HttpSession session){
+        userService.userRegister(user.getUserName(),user.getPassword(),user.getPhone());
+        Integer userId = userService.userLogin(user.getUserName(),user.getPassword());
         session.setAttribute("userId",userId);
         return userId;
     }
