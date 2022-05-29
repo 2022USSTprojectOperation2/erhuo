d36502d16ce21d6231a48b333a211983b041ce4e
Pvneuma <310256126@qq.com>
2022-05-29 18:46:25 +0800

    添加导航栏

--- src/main/java/com/usst/erhuo/service/impl/ImageServiceImpl.java
+++ src/main/java/com/usst/erhuo/service/impl/ImageServiceImpl.java
@@ -3,10 +3,12 @@ package com.usst.erhuo.service.impl;
 import com.usst.erhuo.service.ImageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -22,61 +24,63 @@ public class ImageServiceImpl implements ImageService {
     @Value("${img-path.server}")
     private String serverPath;
 
+    private String getUserDir(){
+        return System.getProperty("user.dir");
+    }
+
     @Override
     public String addCover(MultipartFile file, Integer userId) {
         String fileName=getFileName(file.getOriginalFilename());
-        String visibleUri=serverPath+"cover/"+userId+"/"+fileName;
-        String localUri=localPath+"cover/"+userId+"/"+fileName;
-        File localFile=new File(localUri);
-        if(!localFile.exists()){
-            localFile.mkdirs();
-        }
+        File path= null;
         try {
-            file.transferTo(localFile);
+            path = new File(ResourceUtils.getURL("classpath:").getPath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(!path.exists()){
+            path=new File("");
+        }
+        File upLoad=new File(path.getAbsolutePath(),"static/img/cover/"+userId+"/"+fileName);
+        if(!upLoad.exists()){
+            upLoad.mkdirs();
+        }
+        System.out.println("路径为"+upLoad.getAbsolutePath());
+        String visibleUri=serverPath+"cover/"+userId+"/"+fileName;
+        try {
+            file.transferTo(upLoad);
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        String targetUri=targetPath+"cover/"+userId+"/"+fileName;
-//        File targetFile=new File(targetUri);
-//        if(!targetFile.exists()){
-//           targetFile.mkdirs();
-//        }
-//        try {
-//            file.transferTo(targetFile);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         return visibleUri;
     }
 
     @Override
     public String addDetails(MultipartFile file, Integer userId) {
         String fileName=getFileName(file.getOriginalFilename());
-        String visibleUri=serverPath+"details/"+userId+"/"+fileName;
-        String localUri=localPath+"details/"+userId+"/"+fileName;
-        File localFile=new File(localUri);
-        if(!localFile.exists()){
-            localFile.mkdirs();
-        }
+        File path= null;
         try {
-            file.transferTo(localFile);
+            path = new File(ResourceUtils.getURL("classpath:").getPath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(!path.exists()){
+            path=new File("");
+        }
+        File upLoad=new File(path.getAbsolutePath(),"static/img/details/"+userId+"/"+fileName);
+        if(!upLoad.exists()){
+            upLoad.mkdirs();
+        }
+        String visibleUri=serverPath+"cover/"+userId+"/"+fileName;
+        try {
+            file.transferTo(upLoad);
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        String targetUri=targetPath+"details/"+userId+"/"+fileName;
-//        File targetFile=new File(targetUri);
-//        if(!targetFile.exists()){
-//            targetFile.mkdirs();
-//        }
-//        try {
-//            file.transferTo(targetFile);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         return visibleUri;
     }
 
     private String getFileName(String originalName){
+        System.out.println(originalName);
         return UUID.randomUUID().toString()+"_"+originalName;
     }
 }
